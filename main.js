de95539ecb46432a0c2f7512834349b2754c8779
Jusoso <js4_003@outlook.com>
2024-09-28 13:11:38 -0300

    Initial commit: Add JSON and LocalStorage
    JSON and LocalStorage where added, now all products are objects initialized in JavaScript and not in the index.html

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const checkPs4 = document.getElementById("ps4");
-const checkPs5 = document.getElementById("ps5");
-const checkXboxOne = document.getElementById("xboxone");
-const checkXboxSeriesX = document.getElementById("xboxx");
-const checkWiiU = document.getElementById("wiiu");
-const checkSwitch = document.getElementById("switch");
 const calcBtn = document.getElementById("calcBtn");
 const result = document.getElementById("result");
 const searchInput = document.getElementById("search");
@@ -11,14 +5,11 @@ const createBtn = document.getElementById("createBtn");
 const productName = document.getElementById("productName");
 const productPrice = document.getElementById("productPrice");
 const productsContainer = document.getElementById("productsContainer");
+const resetBtn = document.getElementById("resetBtn");
+const objectError = document.getElementById("objectError");
 let total = 0;
 
-const checkArray = [checkPs4
-    , checkPs5
-    , checkXboxOne
-    , checkXboxSeriesX
-    , checkWiiU
-    , checkSwitch];
+const checkArray = [];
 
     const calcularTotal = (event) =>{
         event.preventDefault();
@@ -64,24 +55,113 @@ class Product{
         this.price = price;
     }
 };
-
-const createProduct = () =>{
-const userProduct = new Product(productName.value, productPrice.value);
+const initialProducts = [
+    new Product("Playstation 4", 250),
+    new Product("Playstation 5", 500),
+    new Product("Xbox One", 250),
+    new Product("Xbox Series X", 500),
+    new Product("Wii U", 250),
+    new Product("Nintendo Switch", 500)
+];
+const generateProduct = (userProduct) =>{
 const productDiv = document.createElement("div");
 productDiv.setAttribute("class", "product");
+productDiv.setAttribute("id", userProduct.name + "-div")
+
 const productLabel = document.createElement("label");
 productLabel.setAttribute("for", userProduct.name);
 productLabel.innerHTML = `${userProduct.name} US$ ${userProduct.price}`;
+
 const productCheckbox = document.createElement("input");
 productCheckbox.setAttribute("type", "checkbox");
 productCheckbox.setAttribute("id", userProduct.name);
 productCheckbox.setAttribute("value", userProduct.price);
+
+const deleteBtn = document.createElement("button");
+deleteBtn.setAttribute("type", "button");
+deleteBtn.setAttribute("id", userProduct.name + "-deleteBtn")
+deleteBtn.setAttribute("class", "dangerBtn");
+deleteBtn.innerHTML = "Delete";
+
 productDiv.appendChild(productLabel);
 productDiv.appendChild(productCheckbox);
+productDiv.appendChild(deleteBtn);
 productsContainer.appendChild(productDiv);
+
 checkArray.push(productCheckbox);
-productName.value = "";
-productPrice.value = "";
+
+deleteBtn.addEventListener("click", ()=>{
+let products = localStorageProductsLoad();
+
+products = products.filter(product => product.name !== userProduct.name);
+localStorageProductsSave(products);
+
+productDiv.remove();
+
+checkArray.splice(checkArray.indexOf(productCheckbox), 1);
+
+result.textContent = "";
+});
+}
+const preConstruct = () =>{
+    
+    let products = localStorageProductsLoad();
+    
+    if(products.length === 0){
+        products = initialProducts;
+        localStorageProductsSave(products);
+    }
+
+    products.forEach((product) =>{
+        generateProduct(product);
+    })
+}
+const reset = () =>{
+    productsContainer.innerHTML = "";
+
+    checkArray.length = 0;
+
+    const products = initialProducts;
+    localStorageProductsSave(products);
+   
+    products.forEach((product) =>{
+        generateProduct(product);
+    });
+
 }
 
+
+const localStorageProductsSave = (products) => {
+    localStorage.setItem("products", JSON.stringify(products));
+};
+
+const localStorageProductsLoad = () => {
+const localStorageProducts = localStorage.getItem("products");
+if(localStorageProducts){
+    return JSON.parse(localStorageProducts);
+}
+return [];
+};
+
+const createProduct = () => {
+
+    if(productName.value.length === 0 || isNaN(productPrice.value) || productPrice.value === ""){
+        objectError.textContent = "Por favor, ingrese parametros validos."
+        return;
+    }
+    console.log(productPrice.value);
+
+    const userProduct = new Product(productName.value, productPrice.value);
+    let products = localStorageProductsLoad();
+    products.push(userProduct);
+    localStorageProductsSave(products);
+    generateProduct(userProduct);
+    productName.value = "";
+    productPrice.value = "";
+};
+
+preConstruct();
+
 createBtn.addEventListener("click", createProduct);
+
+resetBtn.addEventListener("click", reset);
